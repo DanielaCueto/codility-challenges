a6c7b3f8ff216808507ee3553e6482601056a4f7
Daniela <danisalazarcueto7@gmail.com>
2022-10-20 12:24:24 +0100

    add second exercise from the book eloquent js

---
+++ Eloquent-javascript/exercices/02-isEven.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//Ejercicio2: Define una función recursiva esPar que corresponda a esta descripcion
+
+/* const n = 0;
+function esPar(n) {
+  if (n < 0) {
+    return undefined;
+  } else {
+    if (n === 0) {
+      return true;
+    }
+    if (n === 1) {
+      return false;
+    }
+    return esPar(n - 2);
+  }
+}
+
+console.log(esPar(-1)); */
+
+//Another solution
+/* function isEven(n) {
+  if (n < 0) {
+    return undefined;
+  }
+  if (n === 0) {
+    return true;
+  }
+  if (n === 1) {
+    return false;
+  } else {
+    return isEven(n - 2);
+  }
+}
+
+console.log(isEven(75));
+ */
